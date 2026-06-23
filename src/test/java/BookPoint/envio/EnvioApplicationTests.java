83af491006fe1eb005e8bafaac15be9196dd1aa2
Dani <da.picarte@duocuc.cl>
2026-06-23 19:18:43 -0400

    create bd y resource

--- src/test/java/BookPoint/envio/EnvioApplicationTests.java
+++ src/test/java/BookPoint/envio/EnvioApplicationTests.java
@@ -1,5 +1,7 @@
 package BookPoint.envio;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -10,4 +12,8 @@ class EnvioApplicationTests {
 	void contextLoads() {
 	}
 
+	@Test
+	void mainClassExist(){
+		assertNotNull(EnvioApplication.class);
+	}
 }
